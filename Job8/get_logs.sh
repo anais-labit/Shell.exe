d7d673eb075a8029bbb13875c461cbce549076e5
anais-labit <anais.labit@gmail.com>
2022-09-30 17:59:30 +0200

    change_variables_names

--- Job8/get_logs.sh
+++ Job8/get_logs.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 #definir variables
-file="number_connection-$(date +%d-%m-%Y-%H:%M)"
+FILE="number_connection-$(date +%d-%m-%Y-%H:%M)"
 
 #récupérer connexions session, compter le nb de lignes 
 #et envoyer l'output dans un fichier dont la date/heure change
-grep "gdm-password]: pam_unix(gdm-password:session):" /var/log/auth.log | wc -l > $file
+grep "gdm-password]: pam_unix(gdm-password:session):" /var/log/auth.log | wc -l > $FILE
 
 #créer un dossier Backup s'il n'existe pas déjà (-p)
 mkdir -p /home/anais/bin/Shell.exe/Job8/Backup/
 
 #archiver le fichier en lui donnant le nom de son paramètre1 
 #et l'envoyer dans Backup
-tar -cf /home/anais/bin/Shell.exe/Job8/Backup/$file.tar $file 
+tar -cf /home/anais/bin/Shell.exe/Job8/Backup/$FILE.tar $FILE 
 
 #supprimer les fichiers textes inutiles
-rm $file
+rm $FILE
--- Job8/get_todays_logs.sh
+++ Job8/get_todays_logs.sh
@@ -5,22 +5,22 @@
 #ICI, LE CRONTAB EST PROGRAMMÉ UNE FOIS PAR JOUR (à 05h00)
 
 # définir les variables
-today=$(date "+%d")
-file=("number_todays_connection-$(date +"%d-%m-%Y-%H:%M")")
+TODAY=$(date "+%d")
+FILE=("number_todays_connection-$(date +"%d-%m-%Y-%H:%M")")
 
 #récupérer connexions anais
 last | grep anais > anais_logs
 
 #récupérer connexions du jour
-grep $today anais_logs | wc -l > $file
+grep $TODAY anais_logs | wc -l > $FILE
 
 #créer un dossier Backup s'il n'existe pas déjà (-p)
 mkdir -p /home/anais/bin/Shell.exe/Job8/Backup/
 
 #archiver le fichier en lui donnant le nom de son paramètre1 
 #et l'envoyer dans Backup
-tar -cf /home/anais/bin/Shell.exe/Job8/Backup/$file.tar $file
+tar -cf /home/anais/bin/Shell.exe/Job8/Backup/$FILE.tar $FILE
 
 #supprimer les fichiers textes inutiles
-rm $file
+rm $FILE
 rm anais_logs
